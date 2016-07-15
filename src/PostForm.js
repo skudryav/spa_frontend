d751681200099b23e0fd5496c5f7b17c8c37aead
skudryav <SergeKudryavtsev@gmail.com>
2016-07-14 23:27:13 -0400

    changes in PostForm component

--- src/PostForm.js
+++ src/PostForm.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 import Post from './Post';
 
 export default class PostForm1 extends Component {
-  propTypes: {
-     name: React.PropTypes.string.isRequired,
-     description: React.PropTypes.string
-  };
   render() {
     return (
       <div>
